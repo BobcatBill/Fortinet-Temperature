eb3e0bd211a4393ae7187bf4a0f43c574f6e3d99
Bill <BobcatBill@users.noreply.github.com>
2024-07-03 22:49:37 -0500

    Update fortinet-temperature.sh

--- fortinet-temperature.sh
+++ fortinet-temperature.sh
@@ -3,12 +3,11 @@ SNMPSTRING=public
 
 for HOSTNAME in `grep -E "fw" /etc/xymon/hosts.cfg | awk '{print $2}'`; do
 	echo "\tSensor Name\t\tCelsius\tFahrenheit" > /tmp/$HOSTNAME.temp
-	for TARGET in $(snmpwalk -Oq -v2c -c $SNMPSTRING $HOSTNAME 1.3.6.1.4.1.12356.101.4.3); do
- 		if [ "$?" != 0 ]; then
-			continue
+	for TARGET in $(snmpwalk -Oq -v2c -c $SNMPSTRING $HOSTNAME 1.3.6.1.4.1.12356.101.4.3 2>/dev/null | grep -i tempera | awk '{print $1}' ); do
+		if [ "$TARGET" = "" ]; then
+  			echo "Error collecting host $HOSTNAME"
+ 			continue
    		fi
- 		echo "TARGET = '$TARGET'"
-   		TARGET=$(echo "$TARGET" | grep -i tempera | awk '{print $1}' )
 		SENSOR=$(snmpget -Oqv -v2c -c $SNMPSTRING $HOSTNAME $TARGET | sed 's/"//g' | sed 's/ /_/g')
 		TARGET2=$(echo $TARGET | sed 's/.101.4.3.2.1.2./.101.4.3.2.1.3./g')
 		TEMPERATURE=$(snmpget -Oqv -v2c -c $SNMPSTRING $HOSTNAME $TARGET2 | sed 's/"//g')
