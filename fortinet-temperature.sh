5a6fd0a4dbdd26940ff8c0eebb998802ea4915e8
Bill <BobcatBill@users.noreply.github.com>
2024-07-03 21:41:03 -0500

    Update fortinet-temperature.sh

--- fortinet-temperature.sh
+++ fortinet-temperature.sh
@@ -6,6 +6,7 @@ for HOSTNAME in `grep -E "fw" /etc/xymon/hosts.cfg | awk '{print $2}'`; do
   for TARGET in $(snmpwalk -Oq -v2c -c $SNMPSTRING $HOSTNAME 1.3.6.1.4.1.12356.101.4.3 | grep -i tempera | awk '{print $1}' ); do
 		SENSOR=$(snmpget -Oqv -v2c -c $SNMPSTRING $HOSTNAME $TARGET | sed 's/"//g' | sed 's/ /_/g')
 		if [ "$SENSOR" = "" ]; then
+  			rm /tmp/$HOSTNAME.temp
 			break
 		fi
 		TARGET2=$(echo $TARGET | sed 's/.101.4.3.2.1.2./.101.4.3.2.1.3./g')
@@ -17,4 +18,5 @@ for HOSTNAME in `grep -E "fw" /etc/xymon/hosts.cfg | awk '{print $2}'`; do
 	$BB $BBDISP "status $HOSTNAME.temperature green `date`
 `cat /tmp/$HOSTNAME.temp`
 "
+rm /tmp/$HOSTNAME.temp
 done
